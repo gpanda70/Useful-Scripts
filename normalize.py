630c25f0dc624f3f32433ed45d338e347cb1aca5
gpanda70 <gpanda@uga.edu>
2018-09-11 14:11:59 -0400

    Changed num and nums to id and ids

--- normalize.py
+++ normalize.py
@@ -1,4 +1,4 @@
-import numpy  # Numpy is faster than generic python lists
+import numpy as np  # Numpy is faster than generic python lists
 import timeit
 """
 
@@ -6,22 +6,21 @@ This Program normalizes account ids by removing whitespace and leading 000's
 
 """
 lsz = ['000123 12', 'This is a cool program ', 'sdasdas sd asdasd asda ', '0102910920192019033913910310391039']
-def normalize(nums):
+def normalize(ids):
     normalized_list = []
-    for num in nums:
-        num = num.replace(' ', '')  # removes whitespace
-        num = num.lstrip('0')  # removes leading 0's
-        normalized_list.append(num)
+    for id in ids:
+        id = id.replace(' ', '')  # removes whitespace
+        id = id.lstrip('0')  # removes leading 0's
+        normalized_list.append(id)
 
     return (normalized_list)
 
-def np_normalize(nums):
-    import numpy as np
+def np_normalize(ids):
     normalized_list = np.array([])
-    for num in np.array(nums):
-        num = num.replace(' ', '')
-        num = num.lstrip('0')  # removes leading 0's
-        normalized_list = np.append(normalized_list, num)
+    for id in np.array(ids):
+        id = id.replace(' ', '')
+        id = id.lstrip('0')  # removes leading 0's
+        normalized_list = np.append(normalized_list, id)
 
     return (normalized_list)
 
@@ -29,4 +28,4 @@ if __name__ == "__main__":
 
     cy = timeit.timeit('normalize(lsz)', setup='from __main__ import normalize, lsz', number=1000)
     py = timeit.timeit('np_normalize(lsz)', setup='from __main__ import np_normalize, lsz', number=1000)
-    print('numpy is {}x faster than lists'.format(py/cy) )
+    print('idpy is {}x faster than lists'.format(py/cy) )
